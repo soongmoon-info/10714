1e9b635b4e4ff0035a4682f1641ffd285bb78fb9
jonghoan43 <anjongho0804@gmail.com>
2024-09-19 14:47:33 +0900

    Add files via upload

---
+++ 10714-main/pycoding03/learning01.py
@@ -0,0 +1,12 @@
+templist = [10,20,30]
+#추가
+templist.append(40)
+print(templist)
+templist.insert(2, 25)
+print(templist)
+#삭제
+templist.remove(40)
+print(templist)
+del(templist[2])#다 값에 관한거 하지만 이친구는 인덱스 위치에 관한것
+print(templist)
+# 1: 3 ====1이상 3미만
---
+++ 10714-main/pycoding03/learning02.py
@@ -0,0 +1,6 @@
+import numpy as np
+tempArr = np.array([10,20,30])
+#list와 array의 차이 배열과 리스트
+templist = [10,20,30]
+print(tempArr*2)
+print(templist*2)
