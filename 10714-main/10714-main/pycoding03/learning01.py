fed425ae480c6f4b731406c3784755afdaaf6e82
jonghoan43 <anjongho0804@gmail.com>
2024-09-23 08:50:38 +0900

    Add files via upload

--- 10714-main/10714-main/pycoding03/learning01.py
+++ 10714-main/10714-main/pycoding03/learning01.py
@@ -1,11 +1,12 @@
-
-
-
-a=10
 templist = [10,20,30]
-
-templist[0]=1
-templist[1]=2
-templist[2]=3
-
+#추가
+templist.append(40)
 print(templist)
+templist.insert(2, 25)
+print(templist)
+#삭제
+templist.remove(40)
+print(templist)
+del(templist[2])#다 값에 관한거 하지만 이친구는 인덱스 위치에 관한것
+print(templist)
+# 1: 3 ====1이상 3미만
